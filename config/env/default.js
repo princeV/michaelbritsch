3ee82f599479cd9c3280bea1133b593bdcf02a3d
priinceV <F_Hummel@gmx.de>
2015-04-12 14:06:20 +0200

    initialise

---
+++ config/env/default.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+module.exports = {
+	app: {
+		title: 'MichaelBritsch',
+		description: 'Regisseur',
+		keywords: 'Regisseur',
+		googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions'
+};
