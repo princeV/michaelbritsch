b18af93fbc1a4c3f3fdefd017dda020a5add1a88
priinceV <F_Hummel@gmx.de>
2015-06-28 15:22:57 +0200

    set config  vars

--- config/env/secure.js
+++ config/env/secure.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: 443,
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/michaelbritsch',
+	db: process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/michaelbritsch',
 	assets: {
 		lib: {
 			css: [
